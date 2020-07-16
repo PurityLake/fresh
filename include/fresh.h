2ca9289122333031370dc29fc522f5fdae539698
Robert O'Shea <robertoshea2k11@gmail.com>
2020-07-16 22:44:12 +0100

    Fixed build issues

---
+++ include/fresh.h
@@ -0,0 +1,4 @@
+#ifndef __H_FRESH__
+#define __H_FRESH__
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+#include "fresh.h"
+#include "fresh_config.h"
+
 int main(int argc, char **argv) {
-    printf("Hello World!");
+    printf("Hello Fresh v%d.%d!", FRESH_VERSION_MAJOR, FRESH_VERSION_MINOR);
     return 0;
 }
